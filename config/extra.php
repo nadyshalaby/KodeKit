09b086a9bba029bc929636f9e153fe10f4890e85
nady shalaby <nady80878@gmail.com>
2016-07-11 17:41:55 +0200

    Update extra.php

--- config/extra.php
+++ config/extra.php
@@ -10,13 +10,13 @@ return [
     ],
     'social' => [
         'facebook' => [
-            'app_id' => '628866373922560',
-            'app_secret' => '5744a682604b22be38db75b5498e51a9',
+            'app_id' => 'xxxxxxxxxxxxxxxxxxxxxx',
+            'app_secret' => 'xxxxxxxxxxxxxxxxxxxxxx',
             'default_graph_version' => 'v2.5',
         ],
         'google' => [
-            'client_id' => '455053498627-vh4prtd0ap51thgeonfmg0vle2gfjvu9.apps.googleusercontent.com',
-            'client_secret' => 'z2Hrjtv-WRoEBzHlMAuNFUUt',
+            'client_id' => 'xxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com',
+            'client_secret' => 'xxxxxxxxxxxxxxxxxxxxxx',
             'scopes' => 'email'
         ]
     ]
